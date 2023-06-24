ef57014f91c5e35325d3d8ac0d4f9f4091ecce2d
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2023-06-24 18:07:20 +0900

    keep a game record

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
   (() => {
-    let kifu = null;
+    const kifu = [];
     const board = document.querySelector(".board");
     let counter = 0;
 
@@ -8,6 +8,8 @@ window.addEventListener("load", () => {
       const tile = document.querySelector(".board tr > td:hover");
 
       if (tile === null) return;
+
+      kifu.push({ row: tile.dataset.row, column: tile.dataset.column, color: counter % 2 });
 
       tile.style.backgroundColor = counter % 2 === 0 ? "beige" : "darkkhaki";
       counter++;
