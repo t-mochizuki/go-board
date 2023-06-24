eb4fccd16673332cdad0161ae69115e78e13d844
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2023-06-25 05:54:21 +0900

    remove IIFE

--- docs/main.js
+++ docs/main.js
@@ -1,24 +1,21 @@
 window.addEventListener("load", () => {
-  (() => {
-    const kifu = [];
-    const board = document.querySelector("table.board");
+  let counter = 0;
+  const color = () => counter % 2 === 1 ? "WHITE" : "BLACK";
+  const inc = () => counter++;
 
-    let counter = 0;
-    const color = () => counter % 2 === 1 ? "WHITE" : "BLACK";
-    const inc = () => counter++;
+  const kifu = [];
+  const board = document.querySelector("table.board");
+  board.addEventListener("click", () => {
+    const tile = document.querySelector("table.board tr > td:hover");
 
-    board.addEventListener("click", () => {
-      const tile = document.querySelector("table.board tr > td:hover");
+    if (tile === null) return;
 
-      if (tile === null) return;
+    if (tile.dataset.color !== undefined) return;
 
-      if (tile.dataset.color !== undefined) return;
+    tile.dataset.color = color();
 
-      tile.dataset.color = color();
+    kifu.push({ row: tile.dataset.row, column: tile.dataset.column, color: color() });
 
-      kifu.push({ row: tile.dataset.row, column: tile.dataset.column, color: color() });
-
-      inc();
-    });
-  })();
+    inc();
+  });
 });
