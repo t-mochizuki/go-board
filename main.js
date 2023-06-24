522972389e016da90660c69d83fd0ea61f21dca2
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2023-06-24 19:59:26 +0900

    disable players to place a stone if there is occupied

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 window.addEventListener("load", () => {
   (() => {
     const kifu = [];
+    const occupied = [];
+    for (let i = 0; i < 5; ++i) occupied.push(Array(5).fill(false));
     const board = document.querySelector(".board");
     let counter = 0;
 
@@ -8,6 +10,9 @@ window.addEventListener("load", () => {
       const tile = document.querySelector(".board tr > td:hover");
 
       if (tile === null) return;
+
+      if (occupied[parseInt(tile.dataset.row)][parseInt(tile.dataset.column)]) return;
+      else occupied[parseInt(tile.dataset.row)][parseInt(tile.dataset.column)] = true;
 
       kifu.push({ row: tile.dataset.row, column: tile.dataset.column, color: counter % 2 === 1 ? 'WHITE' : 'BLACK' });
 
