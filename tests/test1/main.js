a6a5ae5f3de96c0d3bf2b1c4cd422a74f903d4a5
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2023-06-25 07:01:50 +0900

    add an E2E test

---
+++ tests/test1/main.js
@@ -0,0 +1,30 @@
+// A player can't place a white stone on [2, 2] of the board under the Go rule.
+window.addEventListener("load", () => {
+  [[1, 2], [2, 3], [2, 1], [3, 2]].forEach(([row, column]) => {
+    const tile = document.querySelector(`table.board tr > td[data-row="${row}"][data-column="${column}"]`);
+    tile.dataset.color = "BLACK"
+  });
+
+  // TODO: implement a checker
+
+  let counter = 1;
+  const color = () => counter % 2 === 1 ? "WHITE" : "BLACK";
+  const inc = () => counter++;
+
+  const kifu = [];
+
+  const board = document.querySelector("table.board");
+  board.addEventListener("click", () => {
+    const tile = document.querySelector("table.board tr > td:hover");
+
+    if (tile === null) return;
+
+    if (tile.dataset.color !== undefined) return;
+
+    tile.dataset.color = color();
+
+    kifu.push({ row: tile.dataset.row, column: tile.dataset.column, color: tile.dataset.color });
+
+    inc();
+  });
+});
