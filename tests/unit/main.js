bf04499247b5d61247bfb874f342b0920eac0165
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2023-06-27 23:16:18 +0900

    add tests/unit directory

---
+++ tests/unit/main.js
@@ -0,0 +1,11 @@
+function isRemoval(stones, visited, color, row, column) {
+  if (row < 0 || boardSize <= row) return true;
+  if (column < 0 || boardSize <= column) return true;
+  if (-1 === stones[row][column]) return false;
+  if (color !== stones[row][column]) return true;
+
+  if (visited[row][column]) return true;
+  visited[row][column] = true;
+
+  return [[-1, 0], [0, 1], [1, 0], [-1, 0]].map(([dy, dx]) => isRemoval(stones, visited, color, row + dy, column + dx)).every(x => x);
+}
---
+++ tests/unit/test1/test.js
@@ -0,0 +1,29 @@
+const stones = [];
+const boardSize = 5;
+for (let i = 0; i < boardSize; ++i) stones.push(Array(boardSize).fill(-1));
+
+// 1 0 _
+// 0 _ _
+// _ _ _
+stones[0][0] = 1;
+stones[1][0] = 0;
+stones[0][1] = 0;
+
+{
+  const visited = [];
+  for (let i = 0; i < boardSize; ++i) visited.push(Array(boardSize).fill(false));
+  console.assert(isRemoval(stones, visited, stones[0][0], 0, 0), "The white stone should be removed.");
+  console.table(visited);
+}
+{
+  const visited = [];
+  for (let i = 0; i < boardSize; ++i) visited.push(Array(boardSize).fill(false));
+  console.assert(isRemoval(stones, visited, stones[1][0], 1, 0) === false, "The black stone shouldn't be removed.");
+  console.table(visited);
+}
+{
+  const visited = [];
+  for (let i = 0; i < boardSize; ++i) visited.push(Array(boardSize).fill(false));
+  console.assert(isRemoval(stones, visited, stones[0][1], 0, 1) === false, "The other black stone shouldn't be also not removed.");
+  console.table(visited);
+}
