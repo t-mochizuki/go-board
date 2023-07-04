0220073b058b2cde00466a8d908cf5b13fcde132
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2023-07-04 23:07:20 +0900

    make a few tweaks

--- tests/e2e/test3/setup.js
+++ tests/e2e/test3/setup.js
@@ -1,5 +1,8 @@
 (() => {
-  [[0, 0], [-1, 0], [0, 1], [0, -1], [2, 0], [1, 1], [1, -1]].forEach(([dy, dx]) => {
+  [[0, 0],
+   [-1, 0], [0, 1], [0, -1],
+   ...[[1, 0], [0, 1], [0, -1]].map(([dy, dx]) => [dy + 1, dx])
+  ].forEach(([dy, dx]) => {
     const row = dy + 2;
     const column = dx + 2;
     const tile = document.querySelector(`table.board tr[data-row="${row}"] > td[data-column="${column}"]`);
