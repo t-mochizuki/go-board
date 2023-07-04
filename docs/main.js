2663b510d8eaa1d056e06a31a82479186892a2f0
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2023-07-05 08:59:24 +0900

    There is no table element

--- docs/main.js
+++ docs/main.js
@@ -104,13 +104,9 @@
     }
   }
 
-  let boardSize = 5;
-
-  if (document.querySelector("table.board") === null) {
-    boardSize = 9;
-    const preparer = new BoardPreparer(boardSize);
-    preparer.run();
-  }
+  const boardSize = 9;
+  const preparer = new BoardPreparer(boardSize);
+  preparer.run();
 
   window.addEventListener("load", () => {
     // TODO:
