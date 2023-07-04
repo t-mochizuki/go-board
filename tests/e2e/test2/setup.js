546a2c87a8b80c418a08f725aba19e5e3ce29917
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2023-07-04 22:42:24 +0900

    update tests/e2e/test2

--- tests/e2e/test2/setup.js
+++ tests/e2e/test2/setup.js
@@ -7,4 +7,13 @@
       tile.dataset.color = "BLACK";
     }
   });
+
+  [[0, 1], [1, 0], [0, -1]].forEach(([dy, dx]) => {
+    const row = dy + 3;
+    const column = dx + 2;
+    const tile = document.querySelector(`table.board tr[data-row="${row}"] > td[data-column="${column}"]`);
+    if (tile !== null) {
+      tile.dataset.color = "WHITE";
+    }
+  });
 })();
